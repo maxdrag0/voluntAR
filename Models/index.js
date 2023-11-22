864f858a10d0c96d48cbca082d8708fb0cc83e92
Maximiliano Drago <maximilianoadrago@gmail.com>
2023-11-22 19:01:17 -0300

    Relaciones

--- Models/index.js
+++ Models/index.js
@@ -4,6 +4,16 @@ import TipoSolicitud from "./TipoSolicitud.js";
 import TipoUsuario from "./TipoUsuario.js";
 
 // AGREGAR RELACIONES
+// Relación entre User y TipoUsuario
+User.belongsTo(TipoUsuario, { foreignKey: 'TipoUsuario', as: 'tipoUsuario' });
+TipoUsuario.hasMany(User, { foreignKey: 'TipoUsuario', as: 'usuarios' });
 
+// Relación entre Solicitud y TipoSolicitud
+Solicitud.belongsTo(TipoSolicitud, { foreignKey: 'Tipo', as: 'tipoSolicitud' });
+TipoSolicitud.hasMany(Solicitud, { foreignKey: 'Tipo', as: 'solicitudes' });
+
+// Relación entre Solicitud y User
+Solicitud.belongsTo(User, { foreignKey: 'IdUsuario', as: 'usuario' });
+User.hasMany(Solicitud, { foreignKey: 'IdUsuario', as: 'solicitudes' });
 
 export { User, Solicitud, TipoSolicitud, TipoUsuario };
